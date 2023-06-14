7d864de709e34e8156cd061f5bc88ffad9b6339e
Bradley Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-06-14 16:16:09 +0200

    wrapped randNum in IIFE for immediate execution

--- Exercise1-html_template/Exercise2-guessingGame/script.js
+++ Exercise1-html_template/Exercise2-guessingGame/script.js
@@ -2,26 +2,28 @@ let userGuess = document.getElementById('userGuess');
 let displayGuessResult = document.getElementById('highOrLow');
 let submitBtn = document.getElementById('submitBtn');
 
-
-let randomNumber = () => {
-    let minRange = 1
-    let maxRange = 100
-
+let randomNumber = (() => {
+    let minRange = 1;
+    let maxRange = 100;
+  
     var min = Math.ceil(minRange);
     var max = Math.floor(maxRange);
     return Math.floor(Math.random() * (max - min + 1) + min);
-  }
+  })();
   
 let userGuessCheck = () => {
-    if (userGuess < randomNumber){
+    let guess = parseInt(userGuess.value);
+
+    if (guess < randomNumber){
         displayGuessResult.innerText = `You guessed ${userGuess}, that is too low!`;
-    } else if (userGuess > randomNumber) {
+    } else if (guess > randomNumber) {
         displayGuessResult.innerText = `You guessed ${userGuess}, that is too high!`;
     } else {
         displayGuessResult.innerText = `You guessed ${userGuess}, 
         that is the same as ${randomNumber}, CORRECT!`;
     }
 }
+let random = randomNumber()
 
 submitBtn.addEventListener('click', userGuessCheck)
 
