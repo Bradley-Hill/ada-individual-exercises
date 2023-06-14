22586578fe4427ec0a47d67f361b96f7606a13d6
Bradley Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-06-14 16:38:24 +0200

    changed var to let

--- Exercise1-html_template/Exercise2-guessingGame/script.js
+++ Exercise1-html_template/Exercise2-guessingGame/script.js
@@ -8,8 +8,8 @@ let generateRandomNumber = () => {
     let minRange = 1;
     let maxRange = 100;
   
-    var min = Math.ceil(minRange);
-    var max = Math.floor(maxRange);
+    let min = Math.ceil(minRange);
+    let max = Math.floor(maxRange);
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
 
@@ -19,9 +19,11 @@ let userGuessCheck = () => {
     let guess = parseInt(userGuess.value);
 
     if (guess < randomNumber){
-        displayGuessResult.innerText = `You guessed ${guess}, that is too low!`;
+        displayGuessResult.innerText = `You guessed ${guess}, 
+        that is too low! Guess again...`;
     } else if (guess > randomNumber) {
-        displayGuessResult.innerText = `You guessed ${guess}, that is too high!`;
+        displayGuessResult.innerText = `You guessed ${guess}, 
+        that is too high! Guess again...`;
     } else {
         displayGuessResult.innerText = `You guessed ${guess}, 
         that is the same as ${randomNumber}, CORRECT!`;
