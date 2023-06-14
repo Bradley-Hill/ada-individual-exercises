70498a6a11ae38f88d0e0f0bb8c7d3ec6b3ccbeb
Bradley Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-06-14 16:31:48 +0200

    issue with the lack of persistent number for guess

--- Exercise1-html_template/Exercise2-guessingGame/script.js
+++ Exercise1-html_template/Exercise2-guessingGame/script.js
@@ -2,29 +2,33 @@ let userGuess = document.getElementById('userGuess');
 let displayGuessResult = document.getElementById('highOrLow');
 let submitBtn = document.getElementById('submitBtn');
 
-let randomNumber = (() => {
+
+
+let generateRandomNumber = () => {
     let minRange = 1;
     let maxRange = 100;
   
     var min = Math.ceil(minRange);
     var max = Math.floor(maxRange);
     return Math.floor(Math.random() * (max - min + 1) + min);
-  })();
+  };
+
+let randomNumber = generateRandomNumber();
   
 let userGuessCheck = () => {
     let guess = parseInt(userGuess.value);
 
     if (guess < randomNumber){
-        displayGuessResult.innerText = `You guessed ${userGuess}, that is too low!`;
+        displayGuessResult.innerText = `You guessed ${guess}, that is too low!`;
     } else if (guess > randomNumber) {
-        displayGuessResult.innerText = `You guessed ${userGuess}, that is too high!`;
+        displayGuessResult.innerText = `You guessed ${guess}, that is too high!`;
     } else {
-        displayGuessResult.innerText = `You guessed ${userGuess}, 
+        displayGuessResult.innerText = `You guessed ${guess}, 
         that is the same as ${randomNumber}, CORRECT!`;
     }
 }
-let random = randomNumber()
+
 
 submitBtn.addEventListener('click', userGuessCheck)
 
-  console.log(randomNumber());
+  console.log(randomNumber);
