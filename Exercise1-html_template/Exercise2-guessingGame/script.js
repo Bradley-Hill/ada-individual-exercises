82261c25bd0b2f6f345ea7b5e9363eaf0461ab7c
Bradley Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-06-14 16:08:25 +0200

    addeventlsit to submitBtn

--- Exercise1-html_template/Exercise2-guessingGame/script.js
+++ Exercise1-html_template/Exercise2-guessingGame/script.js
@@ -1,3 +1,8 @@
+let userGuess = document.getElementById('userGuess');
+let displayGuessResult = document.getElementById('highOrLow');
+let submitBtn = document.getElementById('submitBtn');
+
+
 let randomNumber = () => {
     let minRange = 1
     let maxRange = 100
@@ -7,6 +12,17 @@ let randomNumber = () => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
   
-let userGuess = document.getElementById('userGuess');
+let userGuessCheck = () => {
+    if (userGuess < randomNumber){
+        displayGuessResult.innerText = `You guessed ${userGuess}, that is too low!`;
+    } else if (userGuess > randomNumber) {
+        displayGuessResult.innerText = `You guessed ${userGuess}, that is too high!`;
+    } else {
+        displayGuessResult.innerText = `You guessed ${userGuess}, 
+        that is the same as ${randomNumber}, CORRECT!`;
+    }
+}
+
+submitBtn.addEventListener('click', userGuessCheck)
 
   console.log(randomNumber());
