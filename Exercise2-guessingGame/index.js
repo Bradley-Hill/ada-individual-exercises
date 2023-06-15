5b2003dc2b9d5aa9e9a68fbac4705034769b5c8b
Bradley Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-06-15 14:17:15 +0200

    Event lsitener function corrected

--- Exercise2-guessingGame/index.js
+++ Exercise2-guessingGame/index.js
@@ -1,33 +1,49 @@
 const userGuessInput = document.getElementById('userGuess')
+const userButton = document.getElementById('guessBtn')
+
+//Generates random whole number between 1 and 100(inclusive)
+function generateRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    return randomNumber;
+}
+
 
 function inputGivenNumber(){
     let givenNumber = userGuessInput.value
     return givenNumber
 }
 
+// Verify if the givenNumber is a match
+function didIWin(givenNumber){
+    if(givenNumber < randomNumber){
+        console.log(`Too small!`)
+    } else if (givenNumber > randomNumber){
+        console.log(`Too big!`)
+    } else {
+        console.log(`Correct!`)
+    }
+}
+
+let randomNumber = generateRandomNumber(1,100)
+console.log(`The hidden number is ${randomNumber}`)
+
+//Event listener for checking if the givenNumber is correct
+userButton.addEventListener('click', function () {
+    let givenNumber = inputGivenNumber();
+    didIWin(givenNumber);
+  });
+
 function guessGame(){
-    //Generates random whole number between 1 and 100(inclusive)
-    function generateRandomNumber(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-        return randomNumber;
-    }
+    
+    let givenNumber = inputGivenNumber();
+    didIWin(givenNumber)
 
 
-    // Verify if the givenNumber is a match
-    function didIWin(userGuess){
-        console.log(userGuess)
-        console.log(randomNumber)
-    }
-
-    let randomNumber = generateRandomNumber(1,100)
-    console.log(`The hidden number is ${randomNumber}`)
-
-    console.log(inputGivenNumber())
-    let userGuess = inputGivenNumber();
-    didIWin(userGuess);
 
 }
+
+
 
 guessGame()
