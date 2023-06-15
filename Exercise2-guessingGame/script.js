86e351efe19409c5b2753e2b3dedfddbe553b421
Bradley Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-06-15 09:48:24 +0200

    Tidied file system, typos

---
+++ Exercise2-guessingGame/script.js
@@ -0,0 +1,39 @@
+let userGuess = document.getElementById('userGuess');
+let displayGuessResult = document.getElementById('highOrLow');
+let submitBtn = document.getElementById('submitBtn');
+let previousGuesses = document.getElementById('guessedNumbers')
+
+submitBtn.addEventListener('click', userGuessCheck)
+
+let generateRandomNumber = () => {
+    let minRange = 1;
+    let maxRange = 100;
+  
+    let min = Math.ceil(minRange);
+    let max = Math.floor(maxRange);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+let givenNumber = generateRandomNumber();
+
+let userGuessCheck = () => {
+ 
+  if givenNumber === userGuess{
+
+  }
+};
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+  console.log(randomNumber);
