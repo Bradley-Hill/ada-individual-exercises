9f3938dc8a70afcd5df114d12acde8b3c528e030
Bradley Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-06-15 12:30:06 +0200

    Slowly getting there.

--- Exercise2-guessingGame/index.js
+++ Exercise2-guessingGame/index.js
@@ -1,30 +1,33 @@
-const userGuess = document.getElementById('UserGuess')
-const previousGuesses = document.getElementById('guessedNumbers')
-const outputToUser = document.getElementById('highOrLow')
-const buttonGuess = document.getElementById('guessBtn')
+const userGuessInput = document.getElementById('userGuess')
 
-buttonGuess.addEventListener('click', e => {outputToUser.innerHTML = `Here is a clue.`})
-
-// function displayClue(){
-//     outputToUser.innerHTML = `Here is a clue.`
-// }
+function inputGivenNumber(){
+    let givenNumber = userGuessInput.value
+    return givenNumber
+}
 
 function guessGame(){
-
     //Generates random whole number between 1 and 100(inclusive)
     function generateRandomNumber(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+        let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        return randomNumber;
     }
 
-    let givenNumber = 22
-    // let givenNumber = generateRandomNumber(1,100)
 
-    console.log(generateRandomNumber(1,100))
-    console.log(`The hidden number is ${givenNumber}`)
-    console.log('You lose');
+    // Verify if the givenNumber is a match
+    function didIWin(userGuess){
+        console.log(userGuess)
+        console.log(randomNumber)
+    }
+
+    let randomNumber = generateRandomNumber(1,100)
+    console.log(`The hidden number is ${randomNumber}`)
+
+    console.log(inputGivenNumber())
+    let userGuess = inputGivenNumber();
+    didIWin(userGuess);
 
 }
 
-guessGame();
+guessGame()
