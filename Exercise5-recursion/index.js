1d9cf55f593e4c99a626cae431a5de71af874f18
Bradley Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-07-07 12:19:38 +0200

    factorial function works!

--- Exercise5-recursion/index.js
+++ Exercise5-recursion/index.js
@@ -3,24 +3,34 @@ function sumOne(tableOfNumbers){
     let total = 0
     for(let i = 0; i < tableOfNumbers.length; i++){
         total += tableOfNumbers[i]
-        console.log(total)
     }
     console.log(total)
     return total
 }
 
-function sumTwo(tableOfNumbers){
+function sumTwo(tableOfNumbers,i,total=0){
     //recursion
-    let total = 0
-    let i = 0
     if(i >= tableOfNumbers.length){
+        console.log(total)
         return total
     }
     total += tableOfNumbers[i]
-    sumTwo(tableOfNumbers)
+    sumTwo(tableOfNumbers,i+1,total)
+}
+
+function factorialRecursion(numberToFactorise){
+    if(numberToFactorise === 0){
+        return 1
+    }
+    console.log(`The factorial is ${numberToFactorise}`)
+    const result = numberToFactorise * factorialRecursion(numberToFactorise-1)
+
+    return result
 }
 
 let randomTable = [1,2,3,4,5,6,7,8,9,10,11,12]
 
 sumOne(randomTable)
-sumTwo(randomTable)
+sumTwo(randomTable,0)
+let result = factorialRecursion(5)
+console.log(result)
